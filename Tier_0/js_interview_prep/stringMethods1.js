2525314ec8f2a012ed14c267444bf5592a9c94d4
Derek Allred <derekmallred@gmail.com>
2018-12-07 08:59:24 -0700

    added string methods to go over

---
+++ Tier_0/js_interview_prep/stringMethods1.js
@@ -0,0 +1,96 @@
+let string1 = 'Getting to know JS string functions'
+/**
+.charAt = returns a the resulting character at the given index
+ */
+let charAtStr = string1.charAt(0)
+console.log(charAtStr);
+//output: 'G'
+
+/**
+.charCodeAt = 
+ */
+
+ /**
+.concat = 
+ */
+
+
+ /**
+.endsWith = 
+ */
+
+
+ /**
+.fromCharCode = 
+ */
+
+
+ /**
+.includes = 
+ */
+
+
+ /**
+.indexOf = 
+ */
+
+
+ /**
+.lastIndexOf = 
+ */
+
+
+ /**
+.match = 
+ */
+
+
+ /**
+.repeat = 
+ */
+
+
+ /**
+.replace = 
+ */
+
+
+ /**
+.search = 
+ */
+
+
+ /**
+.slice = 
+ */
+
+
+ /**
+.split = 
+ */
+
+
+ /**
+.startsWith = 
+ */
+
+
+ /**
+.substring = 
+ */
+
+
+ /**
+.toLowerCase = 
+ */
+
+
+ /**
+.toUpperCase = 
+ */
+
+
+ /**
+.trim = 
+ */
+
